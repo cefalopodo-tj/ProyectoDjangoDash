043f03e256c04a012a179c2f241aa7273e2d4f73
Cefalopodo <albertolazaroru@gmail.com>
2021-09-10 18:32:48 +0200

    importamos el gráfico creado en simpleexample a las home - urls.py

--- home/urls.py
+++ home/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 from . import views
+#39:45 Para meter la aplicación de dash_apps:
+from home.dash_apps.finished_apps import simpleexample
+#IMPORTANTE ACORDARNOS DE IMPORTAR CUANDO METAMOS MÁS GRAFICOS. Luego vamos a partials-base.html 40:22
 
 urlpatterns = [
     path('', views.home, name='home')

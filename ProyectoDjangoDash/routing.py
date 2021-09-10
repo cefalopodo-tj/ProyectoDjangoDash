c736a3b14b6b1f7a3401f0adf0389fea3f687581
Cefalopodo <albertolazaroru@gmail.com>
2021-09-10 17:29:44 +0200

    Creamos archivo para protocolo

---
+++ ProyectoDjangoDash/routing.py
@@ -0,0 +1,7 @@
+#32:04 importamos el protocolo, con lo que la indexación está OK
+from channels.routing import ProtocolTypeRouter
+
+application=ProtocolTypeRouter({
+
+})
+#Volvemos a settings 32:42

375581cd1e86529283054b86fec1b3793923fbc0
Cefalopodo <albertolazaroru@gmail.com>
2021-09-15 16:29:02 +0200

    Se añade url encuestadores

--- home/urls.py
+++ home/urls.py
@@ -3,10 +3,15 @@ from . import views
 #39:45 Para meter la aplicación de dash_apps:
 from home.dash_apps.finished_apps import simpleexample
 from home.dash_apps.finished_apps import evolution
+from home.dash_apps.finished_apps import table
+
+
 
 #IMPORTANTE ACORDARNOS DE IMPORTAR CUANDO METAMOS MÁS GRAFICOS. Luego vamos a partials-base.html 40:22
 
 urlpatterns = [
-    path('', views.home, name='home')
+    
+    path('', views.home, name='home'),
+    path('encuestadores/', views.home, name='encuestadores'),
 ]
 
